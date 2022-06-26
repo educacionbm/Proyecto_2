ccb0e6f573c214a1ebe10bbfac92efe23ca11854
educacionbm <educacionbm@gmail.com>
2022-06-25 20:25:25 -0400

    Se agrego un retardo para que non consumo muchos recursos del pc

--- 1.SpeechToText/Phase_1.py
+++ 1.SpeechToText/Phase_1.py
@@ -5,6 +5,7 @@ Salidas: texto, del tipo STRING
 
 """
 import speech_recognition as sr
+import time
 
 r = sr.Recognizer() #Instancia reconocedora
 audio_file = sr.AudioFile('Improve your Speaking and Conversational skills with me.wav')
@@ -13,5 +14,6 @@ with audio_file as source: # Creando el audio como fuente para analizar posterio
     audio_analizar = r.record(source)
 
 texto = r.recognize_google(audio_analizar,language='en-EN')
+time.sleep(1.5)
 print(texto)
 
